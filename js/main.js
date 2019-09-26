541a353a93e994445b350b60f56ca0d76c4089ab
Maxim <mkwtzt@gmail.com>
2019-09-26 10:47:59 +0400

    fixs

--- js/main.js
+++ js/main.js
@@ -3,26 +3,25 @@
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
-var getRandomCount = function (min, max) {
+var getRandomInt = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-var getRandomInt = function (max) {
+var getRandomInts = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-var getRandomLength = function (items) {
-  return items[~~(items.length * Math.random())];
+var getRandomElement = function (elements) {
+  return elements[Math.floor(Math.random() * elements.length)];
 };
 
-var PIN_IMG = document.querySelector('.map__pin > img');
 var MAP_PINS = document.querySelector('.map__pins');
 var AD_COUNT = 8;
 var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var OFFER_ROOMS = getRandomCount(1, 5);
-var OFFER_GUESTS = getRandomCount(1, 4);
+var OFFER_ROOMS = getRandomInt(1, 5);
+var OFFER_GUESTS = getRandomInt(1, 4);
 var OFFER_CHECKINS = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_DESCRIPTION = 'Some description text';
@@ -38,35 +37,37 @@ var createAds = function (count) {
     adsObject.offer = {
       title: '600, 350',
       price: 1000,
-      type: OFFER_TYPES[getRandomInt(OFFER_TYPES.length)],
+      type: OFFER_TYPES[getRandomInts(OFFER_TYPES.length)],
       rooms: OFFER_ROOMS,
       guests: OFFER_GUESTS,
-      checkin: OFFER_CHECKINS[getRandomInt(OFFER_CHECKINS.length)],
-      features: OFFER_FEATURES[getRandomLength(OFFER_FEATURES.length)],
+      checkin: OFFER_CHECKINS[getRandomInts(OFFER_CHECKINS.length)],
+      features: OFFER_FEATURES[getRandomElement(OFFER_FEATURES.length)],
       description: OFFER_DESCRIPTION,
-      photos: OFFER_PHOTOS[getRandomLength(OFFER_PHOTOS.length)]
+      photos: OFFER_PHOTOS[getRandomElement(OFFER_PHOTOS.length)]
     };
     adsObject.location = {
-      x: getRandomCount(0, window.outerWidth),
-      y: getRandomCount(130, 630)
+      x: getRandomInt(0, window.outerWidth),
+      y: getRandomInt(130, 630)
     };
     ads[i] = adsObject;
   }
   return ads;
 };
 
-var renderElement = function (array) {
+var pinImg = document.querySelector('.map__pin > img');
+
+var renderElements = function (array) {
   var template = document.querySelector('#pin').content.querySelector('.map__pin');
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < array.length; i++) {
     var element = template.cloneNode(true);
     element.setAttribute('style', 'left: ' + array[i].location.x + 'px; top: ' + array[i].location.y + 'px;');
-    PIN_IMG.setAttribute('src', array[i].autor.avatar);
-    PIN_IMG.setAttribute('alt', array[i].offer.title);
+    pinImg.setAttribute('src', array[i].autor.avatar);
+    pinImg.setAttribute('alt', array[i].offer.title);
     fragment.appendChild(element);
   }
   MAP_PINS.appendChild(fragment);
 };
 
-renderElement(createAds(AD_COUNT));
+renderElements(createAds(AD_COUNT));
