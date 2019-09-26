2c61361c8d9916add36712b27edfd3c0967a8191
Maxim <mkwtzt@gmail.com>
2019-09-26 19:52:37 +0400

    fixs

--- js/main.js
+++ js/main.js
@@ -20,9 +20,7 @@ var getRandomElement = function (elements) {
 var MAP_PINS = document.querySelector('.map__pins');
 var AD_COUNT = 8;
 var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-// var OFFER_ROOMS = getRandomCount(1, 5);
 var OFFER_TITLES = ['Отель Prince', 'Отель APA Asakusa Taharamachi Ekimae', 'Отель APA Hotel Shinagawa Sengakuji Eki-Mae'];
-var OFFER_GUESTS = getRandomCount(1, 4);
 var OFFER_CHECKINS = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OFFER_DESCRIPTIONS = ['Находится рядом с железнодорожным вокзалом Синагава, от которого можно за 25 минут доехать до аэропорта Ханэда.', 'Удобно расположен всего в 1 минуте ходьбы от станции метро Tawaramachi', 'расположен в 2 минутах ходьбы от станции метро Sengakuji', 'расположен в оживленном квартале Кабуки-тё, в 6 минутах ходьбы от железнодорожной станции Shinjuku'];
@@ -36,15 +34,15 @@ var createAds = function (count) {
       avatar: 'img/avatars/user0' + (i + 1) + '.png'
     };
     adsObject.offer = {
-      title: OFFER_TITLES[getRandomElement(OFFER_TITLES.length)],
+      title: getRandomElement(OFFER_TITLES),
       price: getRandomCount(10, 75) * 100,
       type: OFFER_TYPES[getRandomInt(OFFER_TYPES.length)],
       rooms: getRandomCount(1, 5),
-      guests: OFFER_GUESTS,
+      guests: getRandomCount(1, 4),
       checkin: OFFER_CHECKINS[getRandomInt(OFFER_CHECKINS.length)],
-      features: OFFER_FEATURES[getRandomElement(OFFER_FEATURES.length)],
-      description: OFFER_DESCRIPTIONS[getRandomElement(OFFER_DESCRIPTIONS.length)],
-      photos: OFFER_PHOTOS[getRandomElement(OFFER_PHOTOS.length)]
+      features: getRandomElement(OFFER_FEATURES),
+      description: getRandomElement(OFFER_DESCRIPTIONS),
+      photos: getRandomElement(OFFER_PHOTOS.length)
     };
     adsObject.location = {
       x: getRandomCount(0, window.outerWidth),
