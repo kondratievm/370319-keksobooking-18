c176f1bd7b3bd9fca0927e9770d3b7e4a9b11908
Maxim <mkwtzt@gmail.com>
2019-10-27 16:02:50 +0400

    replace files

--- js/cards.js
+++ js/cards.js
@@ -1,25 +1,24 @@
 'use strict';
 
-// Модуль ads.js
+// Модуль cards.js
 (function () {
-  var AD_COUNT = 8;
-
-  // Создаем массив фотографий
-  var createPhotoArr = function (min, max) {
-    var photos = [];
-    for (var i = 0; i < window.getRandomCount(min, max); i++) {
-      photos[i] = 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg';
-    }
-    return photos;
-  };
-
   var OFFER_TYPES = ['flat', 'bungalo', 'house', 'palace'];
   var OFFER_TITLES = ['Отель Prince', 'Отель APA Asakusa Taharamachi Ekimae', 'Отель APA Hotel Shinagawa Sengakuji Eki-Mae'];
   var OFFER_CHECKINS = ['12:00', '13:00', '14:00'];
   var OFFER_CHECKOUTS = ['12:00', '13:00', '14:00'];
   var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var OFFER_DESCRIPTIONS = ['Находится рядом с железнодорожным вокзалом Синагава, от которого можно за 25 минут доехать до аэропорта Ханэда.', 'Удобно расположен всего в 1 минуте ходьбы от станции метро Tawaramachi', 'расположен в 2 минутах ходьбы от станции метро Sengakuji', 'расположен в оживленном квартале Кабуки-тё, в 6 минутах ходьбы от железнодорожной станции Shinjuku'];
-  var OFFER_PHOTOS = createPhotoArr(1, 3);
+  var OFFER_PHOTOS = window.createPhotoArr(1, 3);
+
+  window.cards = {
+    OFFER_TYPES: OFFER_TYPES,
+    OFFER_TITLES: OFFER_TITLES,
+    OFFER_CHECKINS: OFFER_CHECKINS,
+    OFFER_CHECKOUTS: OFFER_CHECKOUTS,
+    OFFER_FEATURES: OFFER_FEATURES,
+    OFFER_DESCRIPTIONS: OFFER_DESCRIPTIONS,
+    OFFER_PHOTOS: OFFER_PHOTOS
+  };
 
   // Создаем объект данных объявлений
   window.createAds = function (count) {
@@ -30,16 +29,16 @@
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       };
       adsObject.offer = {
-        title: window.getRandomElement(OFFER_TITLES),
+        title: window.getRandomElement(window.cards.OFFER_TITLES),
         price: window.getRandomCount(10, 75) * 100,
-        type: window.getRandomElement(OFFER_TYPES),
+        type: window.getRandomElement(window.cards.OFFER_TYPES),
         rooms: window.getRandomCount(1, 5),
         guests: window.getRandomCount(1, 4),
-        checkin: window.getRandomElement(OFFER_CHECKINS),
-        checkout: window.getRandomElement(OFFER_CHECKOUTS),
-        features: window.getRandomElement(OFFER_FEATURES),
-        description: window.getRandomElement(OFFER_DESCRIPTIONS),
-        photos: OFFER_PHOTOS
+        checkin: window.getRandomElement(window.cards.OFFER_CHECKINS),
+        checkout: window.getRandomElement(window.cards.OFFER_CHECKOUTS),
+        features: window.getRandomElement(window.cards.OFFER_FEATURES),
+        description: window.getRandomElement(window.cards.OFFER_DESCRIPTIONS),
+        photos: window.cards.OFFER_PHOTOS
       };
       adsObject.location = {
         x: window.getRandomCount(0, window.outerWidth),
@@ -50,47 +49,18 @@
     return ads;
   };
 
-  // Функция отрисовки фотографий
-  var renderPhotos = function (adPhoto) {
-    var photoList = adPhoto.querySelector('.popup__photos');
-    var photoTemplate = photoList.querySelector('.popup__photo');
-    photoList.innerHTML = '';
-
-    for (var i = 0; i < OFFER_PHOTOS.length; i++) {
-      var photoElement = photoTemplate.cloneNode(true);
-      photoElement.setAttribute('src', OFFER_PHOTOS[i]);
-      photoList.appendChild(photoElement);
-    }
-    adPhoto.appendChild(photoList);
-  };
-
-  // Функция сравнения названий
-  var offerTranslatter = function (type) {
-    switch (type) {
-      case 'flat': type = 'Квартира';
-        break;
-      case 'bungalo': type = 'Бунгало';
-        break;
-      case 'house': type = 'Дом';
-        break;
-      case 'palace': type = 'Дворец';
-        break;
-    }
-    return type;
-  };
-
   // Функция наполнения карточки данными из объекта
-  var renderAd = function (adElement, element) {
+  window.renderAd = function (adElement, element) {
     adElement.querySelector('.popup__avatar').src = element.author.avatar;
     adElement.querySelector('.popup__title').textContent = element.offer.title;
     adElement.querySelector('.popup__text--address').textContent = element.offer.address;
     adElement.querySelector('.popup__text--price').textContent = element.offer.price + '₽/ночь';
-    adElement.querySelector('.popup__type').textContent = offerTranslatter(element.offer.type);
+    adElement.querySelector('.popup__type').textContent = window.offerTranslatter(element.offer.type);
     adElement.querySelector('.popup__text--capacity').textContent = element.offer.rooms + ' комнаты для ' + element.offer.guests + ' гостей';
     adElement.querySelector('.popup__text--time').textContent = 'заезд после ' + element.offer.checkin + ', выезд до ' + element.offer.checkout;
     adElement.querySelector('.popup__features').textContent = element.offer.features;
     adElement.querySelector('.popup__description').textContent = element.offer.description;
-    renderPhotos(adElement);
+    window.renderPhotos(adElement);
   };
 
   // Рендер карточки
@@ -99,12 +69,20 @@
     var template = document.querySelector('#card').content.querySelector('.map__card');
     var fragment = document.createDocumentFragment();
     var element = template.cloneNode(true);
-    renderAd(element, elem);
+    window.renderAd(element, elem);
     fragment.appendChild(element);
 
     adPlace.appendChild(fragment);
   };
 
+  // Функция удаления карточки
+  window.closePopup = function () {
+    var pinCard = document.querySelector('.map__card');
+    if (pinCard) {
+      pinCard.remove();
+    }
+  };
+
   // Передаем в функцию createAds количество элементов
-  window.ads = window.createAds(AD_COUNT);
+  window.ads = window.createAds(window.util.AD_COUNT);
 })();
--- js/forms.js
+++ js/forms.js
@@ -2,34 +2,47 @@
 
 // Модуль forms.js
 (function () {
-  var sectionForm = document.querySelector('.notice');
+  window.sectionForm = document.querySelector('.notice');
+  window.sectionFieldset = window.sectionForm.querySelectorAll('fieldset');
 
-  var sectionFieldset = sectionForm.querySelectorAll('fieldset');
-
-  // Функция деактивации формы
-  var checkDisabled = function () {
-    for (var i = 0; i < sectionFieldset.length; i++) {
-      sectionFieldset[i].setAttribute('disabled', 'disabled');
-    }
-  };
-
-  checkDisabled();
-
-  window.removeDisabled = function () {
-    for (var i = 0; i < sectionFieldset.length; i++) {
-      sectionFieldset[i].removeAttribute('disabled', 'disabled');
-    }
-  };
-
+  var addressInput = document.querySelector('#address');
+  var form = document.querySelector('.ad-form');
   var roomsCount = document.querySelector('#room_number');
   var placesCount = document.querySelector('#capacity');
   var placeNotForGuests = placesCount.querySelectorAll('option')[3];
   var capacityOptions = placesCount.options;
+  var timeIn = document.querySelector('#timein');
+  var timeOut = document.querySelector('#timeout');
+
+  window.forms = {
+    addressInput: addressInput,
+    form: form,
+    roomsCount: roomsCount,
+    placesCount: placesCount,
+    placeNotForGuests: placeNotForGuests,
+    capacityOptions: capacityOptions,
+    timeIn: timeIn,
+    timeOut: timeOut
+  };
+
+  // Функция деактивации формы
+  window.checkDisabled = function () {
+    for (var i = 0; i < window.sectionFieldset.length; i++) {
+      window.sectionFieldset[i].setAttribute('disabled', 'disabled');
+    }
+  };
+
+  // Функция активации формы
+  window.removeDisabled = function () {
+    for (var i = 0; i < window.sectionFieldset.length; i++) {
+      window.sectionFieldset[i].removeAttribute('disabled', 'disabled');
+    }
+  };
 
   // Функция деактивации селекта количества гостей
   var disableAllCapacityOptions = function () {
-    for (var i = 0; i < capacityOptions.length; i++) {
-      capacityOptions[i].setAttribute('disabled', 'disabled');
+    for (var i = 0; i < window.forms.capacityOptions.length; i++) {
+      window.forms.capacityOptions[i].setAttribute('disabled', 'disabled');
     }
   };
 
@@ -37,20 +50,17 @@
 
   // Функция проверки соответствия комнта и гостей
   var changeOption = function () {
-    for (var i = 0; i <= roomsCount.value - 1; i++) {
-      if (roomsCount.value < 100) {
-        capacityOptions[i].removeAttribute('disabled', 'disabled');
-      } else if (roomsCount.value === 100 || i > 3) {
-        placeNotForGuests.removeAttribute('disabled', 'disabled');
+    for (var i = 0; i <= window.forms.roomsCount.value - 1; i++) {
+      if (window.forms.roomsCount.value < 100) {
+        window.forms.capacityOptions[i].removeAttribute('disabled', 'disabled');
+      } else if (window.forms.roomsCount.value === 100 || i > 3) {
+        window.forms.placeNotForGuests.removeAttribute('disabled', 'disabled');
       }
     }
   };
 
-  roomsCount.addEventListener('change', disableAllCapacityOptions);
-  roomsCount.addEventListener('change', changeOption);
-
-  var timeIn = document.querySelector('#timein');
-  var timeOut = document.querySelector('#timeout');
+  window.forms.roomsCount.addEventListener('change', disableAllCapacityOptions);
+  window.forms.roomsCount.addEventListener('change', changeOption);
 
   var changeTimeOption = function (valueA, valueB) {
     switch (true) {
@@ -64,11 +74,11 @@
     return true;
   };
 
-  timeIn.addEventListener('change', function () {
+  window.forms.timeIn.addEventListener('change', function () {
     changeTimeOption(timeIn, timeOut);
   });
 
-  timeOut.addEventListener('change', function () {
+  window.forms.timeOut.addEventListener('change', function () {
     changeTimeOption(timeOut, timeIn);
   });
 })();
