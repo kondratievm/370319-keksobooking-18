1465c65c623aea7eec023655c5e2bf91fa334168
Maxim <mkwtzt@gmail.com>
2019-10-27 17:09:26 +0400

    fix main

--- js/main.js
+++ js/main.js
@@ -1,40 +1,106 @@
 'use strict';
 
-var activatePin = document.querySelector('.map__pin--main');
-var addressInput = document.querySelector('#address');
-var map = document.querySelector('.map');
-var form = document.querySelector('.ad-form');
+// Модуль main.js
+(function () {
+  var activatePin = document.querySelector('.map__pin--main');
+  var map = document.querySelector('.map');
+  var pinTail = parseInt(window.getComputedStyle(document.querySelector('.map__pin--main'), ':after').height, 10);
 
-// Функция открытия страницы
-var openPage = function () {
-  map.classList.remove('map--faded');
-};
+  window.main = {
+    activatePin: activatePin,
+    map: map
+  };
 
-// Функция получения координат главного пина
-var getPinCoords = function () {
-  var mainPinCoords = activatePin.getBoundingClientRect().x + ' ' + activatePin.getBoundingClientRect().y;
+  // Функция открытия страницы
+  window.openPage = function () {
+    window.main.map.classList.remove('map--faded');
+  };
 
-  addressInput.value = mainPinCoords;
-};
+  // Функция получения координат главного пина
+  var getPinCoords = function () {
 
-// Функция активации страницы
-var pageActivate = function () {
-  openPage();
-  form.classList.remove('ad-form--disabled');
+    window.addressCoords = {
+      x: activatePin.offsetLeft + Math.ceil(activatePin.offsetWidth / 2),
+      y: window.main.activatePin.offsetTop + Math.ceil(window.main.activatePin.offsetHeight + pinTail)
+    };
 
-  window.renderPins(window.ads);
-  getPinCoords();
-};
+    window.forms.addressInput.value = window.addressCoords.x + ' ' + window.addressCoords.y;
+  };
 
-activatePin.addEventListener('mousedown', function () {
-  pageActivate();
-  window.removeDisabled();
-});
+  // Функция движения главного пина
+  activatePin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-activatePin.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === window.util.ENTER_KEYCODE) {
-    openPage();
-    pageActivate();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      getPinCoords();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var nextX = activatePin.offsetLeft - shift.x + activatePin.offsetWidth / 2;
+      var nextY = activatePin.offsetTop - shift.y + activatePin.offsetHeight + pinTail;
+
+      var mapRect = map.getBoundingClientRect();
+
+      if (nextX > 0 && nextX < mapRect.width && mapRect.x < moveEvt.clientX && moveEvt.clientX < mapRect.x + mapRect.width) {
+        activatePin.style.left = (activatePin.offsetLeft - shift.x) + 'px';
+      }
+      if (nextY > 130 && nextY < 630 && mapRect.y + 130 < moveEvt.clientY && moveEvt.clientY < mapRect.y + 630) {
+        activatePin.style.top = (activatePin.offsetTop - shift.y) + 'px';
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  var pinActive = false;
+
+  // Функция активации страницы
+  window.pageActivate = function () {
+    window.openPage();
+    window.forms.form.classList.remove('ad-form--disabled');
+
+    window.renderPins(window.ads);
+    getPinCoords();
+  };
+
+  window.main.activatePin.addEventListener('mousedown', function () {
+    getPinCoords();
+
+    if (!pinActive) {
+      window.pageActivate();
+      pinActive = true;
+    }
+
     window.removeDisabled();
-  }
-});
+  });
+
+  window.main.activatePin.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.util.ENTER_KEYCODE) {
+      window.openPage();
+      window.pageActivate();
+      window.removeDisabled();
+    }
+  });
+})();
